70d8279f169c098268efe3e058240043c63d9b7f
Ashton South <aasouth223+github@gmail.com>
2024-05-29 00:09:51 -0400

    Finish reading function

--- loader.py
+++ loader.py
@@ -1,43 +1,29 @@
 from gpiozero import DigitalInputDevice, DigitalOutputDevice
 
-BIT_COUNT = 12
-
 # Define pins
 #TODO: Define some kind of schema?
 #TODO: Define correct pin numbers
 #TODO: Allow some sort of configuration?
-INPUT_PINS = [
-    DigitalInputDevice(0),
-    DigitalInputDevice(1),
-    DigitalInputDevice(2),
-    DigitalInputDevice(3),
-    DigitalInputDevice(4),
-    DigitalInputDevice(5),
-    DigitalInputDevice(6),
-    DigitalInputDevice(7),
-    DigitalInputDevice(8),
-    DigitalInputDevice(9),
-    DigitalInputDevice(10),
-    DigitalInputDevice(11),
-    DigitalInputDevice(12)
-]
+INPUT_PIN_RANGE = [i for i in range(1, 13)]
+INPUT_PINS = [DigitalInputDevice(i) for i in INPUT_PIN_RANGE]
 
-OUTPUT_PINS = [
-    DigitalOutputDevice(13),
-    DigitalOutputDevice(14),
-    DigitalOutputDevice(15),
-    DigitalOutputDevice(16),
-    DigitalOutputDevice(17),
-    DigitalOutputDevice(18),
-    DigitalOutputDevice(19),
-    DigitalOutputDevice(20)
-]
+OUTPUT_PIN_RANGE = [i for i in range(14, 23)]
+OUTPUT_PINS = [DigitalOutputDevice(i) for i in OUTPUT_PIN_RANGE]
 
 class ByteReader:
     def __init__(self, input_pins = INPUT_PINS, output_pins = OUTPUT_PINS):
         self.input_pins = input_pins
         self.output_pins = output_pins
 
-    def read_byte(address):
-        for i in range(BIT_COUNT):
-            self.input_pin
+    def read_byte(self, address):
+        for pin, bit in self.output_pins:
+            up = (address >> bit) & 1
+            if up:
+                pin.on()
+            else:
+                pin.off()
+
+        result = 0
+        for pin, bit in self.input_pins:
+            value = pin.is_active()
+            result += value << bit
